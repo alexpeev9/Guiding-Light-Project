70facbdecf9c7eef629752e5f4709fa7a6f136bc
alexpeev9 <alexpeev9@abv.bg>
2021-07-29 00:59:51 +0300

    Homepage edit

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { CrudService } from 'src/app/services/crud.service';
+import { ErrorHandlerService } from 'src/app/services/error-handler.service';
+import { map } from 'rxjs/operators';
+import { Location } from 'src/app/models/location.model';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +11,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  public errorMessage: string = '';
+  crudService: CrudService<Location>;
+  locations?: Location[];
+  constructor(private db: AngularFireDatabase, private errorHandler: ErrorHandlerService) {
+    this.crudService = new CrudService<Location>("locations", db);
+   }
 
   ngOnInit(): void {
+    this.retrieveLocations();
   }
 
+  retrieveLocations(): void {
+    this.crudService.getAll().snapshotChanges().pipe(
+      map(changes =>
+        changes.map(c =>
+          ({ id: c.payload.key, ...c.payload.val() })
+        )
+      )
+    ).subscribe(data => {
+      this.locations = data;
+    },
+      (error) => {
+        this.errorHandler.handleError(error);
+        this.errorMessage = this.errorHandler.errorMessage;
+      });
+  }
+  
 }
