096f85aa9a091cbf5c98c889d984284db0b9bd2d
alexpeev9 <alexpeev9@abv.bg>
2021-07-31 03:27:12 +0300

    Added Details and get single element crud

--- src/app/components/location/location-details/location-details.component.ts
+++ src/app/components/location/location-details/location-details.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit, Input, OnChanges, Output, EventEmitter } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/database';
+import { ActivatedRoute, Router } from '@angular/router';
 import { YaReadyEvent } from 'angular8-yandex-maps';
+import { map } from 'rxjs/operators';
 
 import { Location } from 'src/app/models/location.model';
 import { CrudService } from 'src/app/services/crud.service';
+import { ErrorHandlerService } from 'src/app/services/error-handler.service';
 
 @Component({
   selector: 'app-location-details',
@@ -11,18 +14,43 @@ import { CrudService } from 'src/app/services/crud.service';
   styleUrls: ['./location-details.component.css']
 })
 export class LocationDetailsComponent implements OnInit, OnChanges {
-
-  @Input() location?: Location;
-  @Output() refreshList: EventEmitter<any> = new EventEmitter();
+  
+  public errorMessage: string = '';
+  id: string ;
+  location?: Location;
   crudService: CrudService<Location>;
+  console: Console;
 
-  constructor(private db: AngularFireDatabase) {
+  constructor(private db: AngularFireDatabase, private router: Router, private activeRoute: ActivatedRoute, private errorHandler: ErrorHandlerService) {
     this.crudService = new CrudService<Location>("locations",db);
+    this.id = this.activeRoute.snapshot.url[1].toString();
+    this.console = window.console;
    }
 
   ngOnInit(): void {
+    this.retrieveLocation();
   }
 
   ngOnChanges(): void {
+  }
+  deleteLocation(): void{
+    this.crudService.delete(this.id).catch(error => {
+      this.errorHandler.handleError(error);
+      this.errorMessage = this.errorHandler.errorMessage;
+    });
+    this.router.navigate(['']);
+  }
+  retrieveLocation(): void {
+    this.crudService.getSingleEl(this.id).snapshotChanges().pipe(
+      map(changes =>
+        changes.payload.toJSON()
+      )
+    ).subscribe(data => {
+      this.location = data as Location;
+    },
+      (error) => {
+        this.errorHandler.handleError(error);
+        this.errorMessage = this.errorHandler.errorMessage;
+      });
   }
 }
--- src/app/services/crud.service.ts
+++ src/app/services/crud.service.ts
@@ -1,15 +1,20 @@
-import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
+import { AngularFireDatabase, AngularFireList, AngularFireObject } from '@angular/fire/database';
 
 export class CrudService<T> {
-
+  private database;
   private dbPath:string;
   private elementRef: AngularFireList<T>;
 
   constructor(value: string, db: AngularFireDatabase) { 
+    this.database = db;
     this.dbPath = `/${value}`;
     this.elementRef = db.list(this.dbPath);
   }
-
+  getSingleEl(id: string): AngularFireObject<T>{
+    let path: string = this.dbPath + `/${id}`;
+    // console.log(this.database.list(path));
+    return this.database.object(path);
+  }
   getAll(): AngularFireList<T> {
     return this.elementRef;
   }
